1b389dd83885c789c9b950a302d2a0ad60a08e9e
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2026-05-12 08:02:10 +0530

    day1 i/p, o/p and dataTypes

---
+++ data_types.py
@@ -0,0 +1,39 @@
+my_integer_var = 10
+print(type(my_integer_var))  # <class 'int'>
+print(my_integer_var)  # 10
+
+my_float_var = 4.50
+print(type(my_float_var))  # <class 'float'>
+print(my_float_var)  # 4.5
+
+my_string_var = 'hello'
+print(type(my_string_var))  # <class 'str'>
+print(my_string_var)  # hello
+
+my_boolean_var = True
+print(type(my_boolean_var))  # <class 'bool'>
+print(my_boolean_var)  # True
+
+my_set_var = {7, 'hello', 8.5}
+print(type(my_set_var))  # <class 'set'>
+print(my_set_var)  # {8.5, 'hello', 7}
+
+my_dictionary_var = {'name': 'Alice', 'age': 25}
+print(type(my_dictionary_var))  # <class 'dict'>
+print(my_dictionary_var)  # {'name': 'Alice', 'age': 25}
+
+my_tuple_var = (7, 'hello', 8.5)
+print(type(my_tuple_var))  # <class 'tuple'>
+print(my_tuple_var)  # (7, 'hello', 8.5)
+
+my_range_var = range(5)
+print(type(my_range_var))  # <class 'range'>
+print(my_range_var)  # range(0, 5)
+
+my_list = [22, 'Hello world', 3.14, True]
+print(type(my_list)) # <class 'list'>
+print(my_list) # [22, 'Hello world', 3.14, True]
+
+my_none_var = None
+print(type(my_none_var)) # <class 'NoneType'>
+print(my_none_var) # None
